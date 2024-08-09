f3dbdaa639ba354b2043b221a8bccc96994d4bea
talgat.abdraimov <talgat.abdraimov@zimran.io>
2024-08-09 20:54:42 +0500

    added additional commands and error handlers

--- code/bot.py
+++ code/bot.py
@@ -23,17 +23,39 @@ def logging(func):
     return wrapper
 
 
+def validate(func):
+    @wraps(func)
+    async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if update.effective_user.is_bot:
+            logger.warning(
+                'User <{username}> is a bot. ',
+                username=update.effective_user.full_name,
+                message=update.message.text,
+            )
+
+            await update.message.reply_text("I don't talk to bots.")
+
+            return
+
+        if emoji.is_emoji(update.message.text):
+            logger.warning(
+                'User <{username}> sent an emoji: {message}',
+                username=update.effective_user.full_name,
+                message=update.message.text,
+            )
+
+            await update.message.reply_text(update.message.text)
+
+            return
+
+        return await func(update, context)
+
+    return wrapper
+
+
 @logging
+@validate
 async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if emoji.is_emoji(update.message.text):
-        logger.warning(
-            f'User <{update.effective_user.full_name or update.effective_user.username}> sent an emoji: {update.message.text}'
-        )
-
-        await update.message.reply_text(update.message.text)
-
-        return
-
     answer = await update.message.reply_text("Your message is in progress. I'll notify you when it's done.")
 
     data = {
@@ -41,10 +63,40 @@ async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         'text': update.message.text,
         'message_id': answer.message_id,
         'chat_id': answer.chat_id,
+        'prompt_name': 'grammar',
     }
 
-    username = update.effective_user.full_name or update.effective_user.username
-    logger.info(f'User <{username}> sent a message: {data["text"]}')
+    context.job_queue.run_once(anthropic_api_call, 1, data=data, chat_id=update.effective_chat.id)
+
+
+@logging
+@validate
+async def summary_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    answer = await update.message.reply_text("Your message is in progress. I'll notify you when it's done.")
+
+    data = {
+        'user_id': update.effective_user.id,
+        'text': ''.join(update.message.text.split('/summary')[1:]).strip(),
+        'message_id': answer.message_id,
+        'chat_id': answer.chat_id,
+        'prompt_name': 'summarize',
+    }
+
+    context.job_queue.run_once(anthropic_api_call, 1, data=data, chat_id=update.effective_chat.id)
+
+
+@logging
+@validate
+async def paraphrase_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    answer = await update.message.reply_text("Your message is in progress. I'll notify you when it's done.")
+
+    data = {
+        'user_id': update.effective_user.id,
+        'text': ''.join(update.message.text.split('/paraphrase')[1:]).strip(),
+        'message_id': answer.message_id,
+        'chat_id': answer.chat_id,
+        'prompt_name': 'paraphrase',
+    }
 
     context.job_queue.run_once(anthropic_api_call, 1, data=data, chat_id=update.effective_chat.id)
 
@@ -55,20 +107,14 @@ async def anthropic_api_call(context: ContextTypes.DEFAULT_TYPE) -> str:
 
     text = job.data['text']
 
-    corrected_message = await anthropic.messages(text)
+    try:
+        corrected_message = await anthropic.messages(job.data['prompt_name'], text)
+    except Exception as e:
+        logger.error('An error occurred: {e}', e=e)
+
+        corrected_message = 'An error occurred while processing your message. Please try again'
 
     await context.bot.editMessageText(corrected_message, job.data['chat_id'], job.data['message_id'])
-
-
-@logging
-async def not_text_handler(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
-    username = update.effective_user.full_name or update.effective_user.username
-
-    logger.warning(
-        'User <{username}> sent a non-text message: {message}', username=username, message=update.message
-    )
-
-    await update.message.reply_text('Please send me a text message.')
 
 
 @logging
@@ -83,12 +129,10 @@ async def start_handler(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
 def run_telegram_bot(token: str):
     app = ApplicationBuilder().token(token).build()
 
-    app.add_handler(
-        CommandHandler('start', start_handler, filters=filters.ChatType.PRIVATE & filters.Command('start'))
-    )
+    app.add_handler(CommandHandler('start', start_handler))
+    app.add_handler(CommandHandler('summary', summary_handler))
+    app.add_handler(CommandHandler('paraphrase', paraphrase_handler))
     app.add_handler(MessageHandler(filters.TEXT, text_handler))
-    app.add_handler(MessageHandler(filters.COMMAND, not_text_handler))
-    app.add_handler(MessageHandler(~filters.TEXT, not_text_handler))
 
     app.run_polling()
 
